cf3250f4c17d488107919dd303e0fc762f3a3c74
moonforever <1990923840@qq.com>
2019-06-27 16:24:04 +0800

    This is my start

---
+++ src/test/java/com/example/etail/EtailApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.etail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EtailApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
